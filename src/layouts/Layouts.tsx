5470fd984d9fa068d7c82d0a10e152aede10dde0
ifnuu01 <ifnuu01@gmail.com>
2025-07-18 14:41:48 +0800

    feat : add layout

--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
 function Layouts() {
     return (
-        <div>
-            <h1>Layouts</h1>
-            <Outlet />
+        <div className="font-family-inknut text-white min-h-screen">
+            <Navbar />
+            <main className="pt-40 px-4 md:px-14">
+                <Outlet />
+            </main>
+            <Footer />
         </div>
     )
 }
