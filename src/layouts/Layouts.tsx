d385359975543934bab05070f797709c4a7f6e5b
ifnuu01 <ifnuu01@gmail.com>
2025-07-19 14:28:10 +0800

    feat: add location state to top

--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -1,11 +1,21 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
+import { useEffect } from "react";
+
 function Layouts() {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (!location.state?.preserveScroll) {
+            window.scrollTo(0, 0);
+        }
+    }, [location.state?.preserveScroll, location.pathname]);
+
     return (
         <div className="font-family-inknut text-white min-h-screen">
             <Navbar />
-            <main className="pt-40 px-4 md:px-14">
+            <main className="pt-40 px-4 md:px-16 md:max-w-[1630px] mx-auto">
                 <Outlet />
             </main>
             <Footer />
